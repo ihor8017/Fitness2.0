451e473fff1fd3fe0a4f8566670e008e17fd193b
Ihor Chyzh <djar80@gmail.com>
2023-07-15 12:04:12 -0500

    add js to ask&question

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,20 @@
 import playVideo from "./video.js";
 import { renderPlan } from "./render-plans.js";
 import swiper from "./vendor/slider.js";
+import {initTabs} from "./vendor/init-tabs.js";
+import {initAccordions} from './vendor/init-accordion.js';
 playVideo();
 renderPlan();
 swiper;
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initTabs();
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initAccordions();
+  });
+});
