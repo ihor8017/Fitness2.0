d03e79be1b9bbeecdd9c20b35502cfcc900aede6
Ihor Chyzh <djar80@gmail.com>
2023-07-22 15:58:19 -0500

    изменяет классы кнопок слайдера

--- source/js/change-class.js
+++ source/js/change-class.js
@@ -2,10 +2,19 @@ const changeClass = function () {
   const slider = document.querySelector(".slider");
   const sliderWrapper = slider.querySelector(".slider__wrapper");
   const sliderSlides = slider.querySelectorAll(".slider__slide");
+  const nextButton = slider.querySelector(".slider__button-next");
+  const prevButton = slider.querySelector(".slider__button-prev");
+
+  nextButton.style.display = "block";
+  prevButton.style.display = "block";
   slider.classList.add("swiper")
   slider.classList.remove("slider");
   sliderWrapper.classList.add("swiper-wrapper");
   sliderWrapper.classList.remove("slider__wrapper");
+  nextButton.classList.add("swiper__button-next");
+  nextButton.classList.remove("slider__button-next");
+  prevButton.classList.add("swiper__button-prev");
+  prevButton.classList.remove("slider__button-prev");
   sliderSlides.forEach(element => {
     element.classList.add("swiper-slide");
     element.classList.remove("slider__slide");
